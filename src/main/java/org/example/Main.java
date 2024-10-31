a9cc52748fa0aabdb03d22f2556d70f50b3d2aac
Simon Forteza <fortezasimon@gmail.com>
2024-10-31 20:28:32 -0300

    creacion de la clase sistema

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,10 @@
 package org.example;
 
-import org.example.model.Altura;
-import org.example.model.AlturaNumerada;
 
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
     }
 }
