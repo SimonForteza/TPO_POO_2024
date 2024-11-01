ae16a08d17347220e7cbb9d62d90459309ffdfc6
Simon Forteza <fortezasimon@gmail.com>
2024-11-01 12:30:22 -0300

    creacion del metodo reservarSector

---
+++ src/main/java/org/example/model/Estadio.java
@@ -0,0 +1,29 @@
+package org.example.model;
+
+import java.util.*;
+
+public class Estadio {
+    private Date fecha;
+    List<Sector>sectores;
+
+    public Estadio(Date fecha) {
+        this.fecha = fecha;
+        sectores = new ArrayList<>();
+
+        sectores.add(new Belgrano("belgrano"));
+        sectores.add(new SanMartin("sanmartin"));
+        sectores.add(new Sivori("sivori"));
+        sectores.add(new Centenario("centenario"));
+    }
+
+    public void reservarSector(String nombreSector, String altura, int numeroAsiento) {
+        for (Sector sector: sectores) {
+            if (nombreSector.equals(sector.getNombreSector())) {
+                sector.reservarAltura(altura, numeroAsiento);
+                return;
+            }
+        }
+        System.out.println("el nombre del sector no se encuentra");
+    }
+
+}
