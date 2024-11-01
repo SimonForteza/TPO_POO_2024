1a7ce86a8dd6693ad002b748cedb74677f611bc1
Simon Forteza <fortezasimon@gmail.com>
2024-11-01 14:45:53 -0300

    agrego atributo precio a la clase Altura

--- src/main/java/org/example/model/Altura.java
+++ src/main/java/org/example/model/Altura.java
@@ -2,10 +2,16 @@ package org.example.model;
 
 public abstract class Altura {
     protected int capacidad;
+    private int precio;
 
-    public Altura(int capacidad) {
+    public Altura(int capacidad, int precio) {
         this.capacidad = capacidad;
+        this.precio = precio;
     }
 
     public abstract boolean reservarLugares(int numeroAsiento);
+
+    public int getPrecio() {
+        return precio;
+    }
 }
